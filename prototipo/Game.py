c06e78dbb4948f0420e9aeb6bbf0197b4e91d097
Arthur de Lara Machado <arthurlaramachado@gmail.com>
2023-05-09 12:21:35 -0300

    initial pygame layout

---
+++ prototipo/Game.py
@@ -0,0 +1,54 @@
+import pygame
+from pygame.locals import *
+import sys
+
+class Game:
+    def __init__(self, config, match): 
+        self.__config = config
+        self.__match = match
+        self.__screen = pygame.display.set_mode((150, 50))  #essa informação deve vir de config
+        self.__background = pygame.Surface(self.__screen.get_size())
+        self.__running = False
+        self.__fps: int = 60
+
+    def start_game(self):
+        pygame.init()
+        self.__running = True
+        fps = pygame.time.Clock()
+        fps.tick(self.__fps)
+        self.loop()
+
+    def loop(self):
+        while self.__running:
+            events = self.handle_events()
+            self.process_input(events)
+            self.draw_frame()
+            self.render()
+
+    def handle_events(self):
+        events = pygame.event.get()
+        for event in events:
+            if event == QUIT:
+                self.quit()
+
+        return events
+    
+    def process_input(self, events):
+        self.__match.process_input(events)
+
+    def draw_frame(self):
+        self.__match.draw()
+
+    def render(self):
+        pygame.display.flip()
+
+    def quit(self):
+        self.__running = False
+        pygame.quit()
+        sys.exit()
+
+    def show_menu(self):
+        pass
+    
+    def end_game(self):
+        pass
---
+++ prototipo/Match.py
@@ -0,0 +1,43 @@
+import pygame
+from pygame.locals import *
+from GameObject import GameObject # to do - implement GameObject class
+
+class Match:
+    def __init__(self):
+        self.__game_objects: list[GameObject] = []
+        self.__cenario = None
+        self.__time: int = 0
+
+    def check_collisions(self):
+        for game_obj in self.__game_objects:
+            game_obj.check_collisions(self.__game_objects)
+
+    def draw_score(self):
+        pass
+    
+    def process_input(self, events):
+        pass
+
+    def draw(self):
+        for obj in self.__game_objects:
+            obj.draw()
+
+        self.draw_score()
+
+    def check_goal(self):
+        #can be implemented with custom event, ex
+        """
+        suppose that the event is called GOAL, and it's implemented as such:
+            GOAL = pygame.USEREVENT + 1
+
+            it can be send to the events list in pygame using:
+            ev = pygame.event.Event(GOAL)
+            pygame.event.post(ev)
+
+            then check_goal() can receive events and then handle the pygame event GOAL
+        """
+        pass
+
+    def update_time():
+        pass
+
