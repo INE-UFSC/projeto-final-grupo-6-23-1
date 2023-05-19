86630098a5bee5ca7e96067cbed1fcfa262f542b
Provavelmente-o-Caio <caiofecardoso@hotmail.com>
2023-05-19 09:52:04 -0300

    goal system (it's broken)

--- prototipo/GameObject.py
+++ prototipo/GameObject.py
@@ -17,6 +17,10 @@ class GameObject:
     def get_pos_y(self) -> int:
         return self.__rect.y
     
+    def set_pos(self, pos_x: int, pos_y: int):
+        self.__rect.x = pos_x
+        self.__rect.y = pos_y
+
     def get_rect(self) -> Rect:
         return self.__rect
     
--- prototipo/Match.py
+++ prototipo/Match.py
@@ -5,6 +5,7 @@ from Player import Player
 from Ball import Ball
 from MovingObjects import MovingObjects
 
+
 class Match:
     def __init__(self):
         self.__game_objects: list[GameObject] = [
@@ -20,12 +21,19 @@ class Match:
         for game_obj in self.__game_objects:
             game_obj.check_collisions(self.__game_objects) """
 
-    def draw_score(self):
+    def draw_time(self):
         self.update_time()
         font = pygame.font.Font(None, 40)
         text = font.render(str(self.__time), False, 'White')
         return text
-    
+
+    def draw_score(self):
+        scores = self.update_score()
+        font = pygame.font.Font(None, 40)
+        text_player1 = font.render(str(scores[0]), False, 'White')
+        text_player2 = font.render(str(scores[1]), False, 'White')
+        return [text_player1, text_player2]
+
     def process_input(self, events, screen):
         for obj in self.__game_objects:
             if isinstance(obj, MovingObjects):
@@ -38,8 +46,7 @@ class Match:
 
     def draw(self, pg: pygame, surface: pygame.Surface):
         surface.fill((0, 0, 0)) #it clears the previous frame to draw a new one
-                                #to do - implement cenario
-        if self.__cenario == 'test':
+        if self.__cenario == 'test': #to do - implement cenario
             background = pygame.image.load('sprites/stages/test/background.png')
             ground = pygame.image.load('sprites/stages/test/ground.png')
         surface.blit(background, (0,0))
@@ -48,7 +55,10 @@ class Match:
         for obj in self.__game_objects:
             obj.draw(pg, surface)
 
-        surface.blit(self.draw_score(), (300,5))
+        surface.blit(self.draw_time(), (300,5))
+
+        surface.blit(self.draw_score()[0], (0, 5))
+        surface.blit(self.draw_score()[1], (0, 60))
 
     def check_goal(self):
         #can be implemented with custom event, ex
@@ -66,3 +76,21 @@ class Match:
 
     def update_time(self):
         pass
+
+    def update_score(self):
+        for obj in self.__game_objects:
+            if isinstance(obj, Ball):
+                if obj.get_pos_x() <= 30:
+                    self.__game_objects[0].add_goals()
+                    self.reset()
+                elif obj.get_pos_x() >= 610:
+                    self.__game_objects[1].add_goals()
+                    self.reset()
+
+        score_player1 = self.__game_objects[0].get_goals()
+        score_player2 = self.__game_objects[1].get_goals()
+        return [score_player1, score_player2]
+    
+    def reset(self):
+        for obj in self.__game_objects:
+            obj.set_pos(320,200)
