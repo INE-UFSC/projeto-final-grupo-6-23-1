e589f1ec4b08cf5505c53e68874b0a10fcc8e6e4
Felipe Pacheco <lipe.fagundespacheco@gmail.com>
2023-05-22 13:13:06 -0300

    B/D in the last player

--- prototipo/Buff.py
+++ prototipo/Buff.py
@@ -21,12 +21,12 @@ class Buff(Collectables):
                 if isinstance(obj, Ball):
                     self.apply_buff(obj)
                     collided = True
-                elif isinstance(obj, Player):
+                """   elif isinstance(obj, Player):
                     self.apply_buff(obj)
                     collided = True
                 elif isinstance(obj, Goalpost):
                     self.apply_buff(obj)
-                    collided = True
+                    collided = True """
 
                 if collided:
                     self.disappear(objects)
@@ -35,24 +35,25 @@ class Buff(Collectables):
 
         return False
     
-        """ def update(self):
+    def update(self):
         if self.start_time is not None:
             elapsed_time = pygame.time.get_ticks() - self.start_time
             if elapsed_time >= self.duration * 1000:  # Verifica se o tempo decorrido é maior ou igual à duração em milissegundos
                 self.remove_buff()
-                self.start_time = None"""
+                self.start_time = None
     
     def gen_rand_buff(self):
-        buffs = ['size_up']
+        buffs = ['size_up_player']
         return random.choice(buffs)
 
-    def apply_buff(self, obj):
-        if self.get_type() == 'size_up':
-            pygame.event.post(pygame.event.Event(BUFF_APPLIED, target=obj))
+    def apply_buff(self, obj: Ball):
+        if self.get_type() == 'size_up_player':
+            player = obj.get_last_touched()
+            pygame.event.post(pygame.event.Event(BUFF_APPLIED, target=player))
     
-    def remove_buff(self):
-        if self.get_type() == 'size_up':
-            pygame.event.post(pygame.event.Event(BUFF_APPLIED, target=None))
+    """ def remove_buff(self):
+        if self.get_type() == 'size_up_player':
+            pygame.event.post(pygame.event.Event(BUFF_APPLIED, target=None)) """
         
     def handle_events(self, events):
         pass
@@ -60,6 +61,3 @@ class Buff(Collectables):
     def draw(self, pg: pygame, surface: pygame.Surface):
         pg.draw.rect(surface, (0, 255, 0), self.get_rect())
     
-    def reset_object(self, obj: list[GameObject]):
-        self.width = 20
-        self.height = 20
