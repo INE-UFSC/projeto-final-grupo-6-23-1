4ab41427de56978f0ae38789cd947b7ac48d8b8a
Arthur de Lara Machado <arthurlaramachado@gmail.com>
2023-06-13 09:07:43 -0300

    add: global image path getter

--- versao_final/utils/__init__.py
+++ versao_final/utils/__init__.py
@@ -1,3 +1,5 @@
+import os
+import inspect
 from GameObject import GameObject
 import pygame
 from pygame.locals import *
@@ -13,3 +15,14 @@ def get_obj_of_type(obj_list: list[GameObject], obj_type) -> list:
             found_objs.append(obj)
 
     return found_objs
+
+def get_image_path(*args):
+    # Get the address of the calling file
+    calling_frame = inspect.stack()[1]
+    calling_module = inspect.getmodule(calling_frame[0])
+    calling_file_dir = os.path.dirname(os.path.abspath(calling_module.__file__))
+
+    path = [calling_file_dir, *args]
+    image_path = os.path.join(*path)
+
+    return image_path
