c516f6311e0dc755420fd4275ca3b772aab846aa
Arthur de Lara Machado <arthurlaramachado@gmail.com>
2023-06-26 14:46:21 -0300

    in progress: fix scenario

--- versao_final/Scenario.py
+++ versao_final/Scenario.py
@@ -4,14 +4,21 @@ from utils import get_file_path
 from Ground import Ground
 
 class Scenario:
-    def __init__(self):
+    def __init__(self, surface):
         self.__ground_image = pygame.image.load(get_file_path('sprites','stages','test','ground.png'))
         self.__ground = Ground(640, 72, 0, 288, self.__ground_image)
+        self.__background = Rect(0, 0, surface.get_width(), surface.get_height() - self.__ground.get_height())
+        self.__background_sprite = pygame.image.load(get_file_path('sprites','stadium.png'))
     
     def get_ground_height(self):
         return self.__ground.get_rect().height
     
     def get_structures(self):
         return [self.__ground]
+    
+    def draw_background(self, surface):
+        rect = self.__background
+        resized_sprite = pygame.transform.scale(self.__background_sprite, (rect.width, rect.height))
+        surface.blit(resized_sprite, rect)
     
 
